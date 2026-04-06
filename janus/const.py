4a938af110c144dba0316aa49cffb80d128b353a
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2026-04-06 13:01:14 -0400

    image change

--- janus/const.py
+++ janus/const.py
@@ -2,4 +2,4 @@
 BRADENSBAY_API_URL = "https://janus.bradensbay.com"
 
 # LXD compute node image fingerprint
-LXD_COMPUTE_NODE_FINGERPRINT = "b03058e361bf"
+LXD_COMPUTE_NODE_FINGERPRINT = "52e2770759f6"
